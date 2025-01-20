1d844bfc33b8e5c1b122b345652cb291c2c1578a
iCaran <pratapmorgan@gmail.com>
2025-01-21 01:15:35 +0530

    increase card size and add tag bubbles

--- frontend/src/components/CardSlider.jsx
+++ frontend/src/components/CardSlider.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 
 const CardSlider = ({ cards }) => {
@@ -8,22 +9,24 @@ const CardSlider = ({ cards }) => {
   };
 
   return (
-    <div className="w-full max-w-xl mx-auto overflow-hidden relative">
-      <div className="h-40 flex items-center justify-center border rounded-lg bg-rigelSalmon shadow-md">
-        <p className="text-gray-800 text-lg px-4 text-center">
+    <div className="w-full max-w-3xl mx-auto overflow-hidden relative">
+      {/* Card */}
+      <div className="min-h-[300px] max-w-full flex items-center justify-center border rounded-lg bg-rigelSalmon shadow-md p-6">
+        <p className="text-gray-1000 font-semibold text-lg leading-relaxed text-center">
           {cards[currentIndex]}
         </p>
       </div>
 
+      {/* Buttons */}
       <div className="flex items-center justify-center mt-4 space-x-4">
         <button
           onClick={handleNext}
-          className="bg-gray-200 text-gray-700 px-4 py-2 rounded-lg hover:bg-gray-300"
+          className="bg-gray-200 font-semibold text-gray-700 px-4 py-2 rounded-lg hover:bg-gray-300"
         >
           Next
         </button>
         <button
-          className="bg-rigelSalmon text-gray-700 px-4 py-2 rounded-lg hover:bg-lightSalmon"
+          className="bg-rigelSalmon font-semibold text-gray-700 px-4 py-2 rounded-lg hover:bg-lightSalmon"
         >
           Reply
         </button>
