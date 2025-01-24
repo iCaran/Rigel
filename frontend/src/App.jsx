5348f96210746ebdc3634286754e473174221beb
divyanshu <divyanshukumar725@gmail.com>
2025-01-24 18:44:24 +0530

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import { Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
 const App = () => {
 
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/auth/login' element={<Login />} />
         <Route path='/auth/SignUp' element={<SignUp />} />
+        <Route path='/Landing' element={<Landing/>}/>
       </Routes>
     </>
   );
